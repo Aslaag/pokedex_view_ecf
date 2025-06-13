bab4935ad768f7f01f824f28beafafb25b115c62
Aslaag <lilice.flo@gmail.com>
2025-06-13 17:24:58 +0200

    chores(project): set up constants

---
+++ src/constants/pokedex-const.jsx
@@ -0,0 +1,6 @@
+export const IMG = Object.freeze({
+  POKEBALL_IMG: "/src/assets/pokeball.png",
+  POKEDEX_IMG: "/src/assets/pokedex.png",
+  BG_IMG: "/src/assets/pokeball_bg.svg",
+  POKEMON_IMG: `${import.meta.env.VITE_POKEMON_IMG}/:id.svg`
+})
--- src/pages/PokedexHomePage.jsx
+++ src/pages/PokedexHomePage.jsx
@@ -1,7 +1,8 @@
+
 export function PokedexHomePage() {
   return (
     <>
-      <h1 className="uppercase">all the pokemons in the woooorld</h1>  
+      <h1 className="uppercase">all the pokemons in the woooorld</h1>
     </>
   )
 }
