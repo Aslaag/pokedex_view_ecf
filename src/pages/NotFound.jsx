990751612c68ced7491140cd05fd19d1eecae06a
Aslaag <lilice.flo@gmail.com>
2025-06-14 19:05:29 +0200

    chore(404): NotFoundPage creation

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,10 @@
+import { IMG } from "../constants/pokedex-const";
+
 export function NotFound() {
   return (
-    <>
-      <h1 className="uppercase">uh oh, no pokemon here...</h1>  
-    </>
+    <div className="flex flex-col items-center pt-10 gap-2">
+      <h2 className="uppercase">uh oh, no pokemon here...</h2>
+      <img src={IMG.IMG_NOT_FOUND} alt="" className="rounded-2xl shadow-2xl" />
+    </div>
   )
 }
