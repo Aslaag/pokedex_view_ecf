f0165d9be44479cc3fb6a88dde79592f6495ac02
Aslaag <lilice.flo@gmail.com>
2025-06-15 17:59:07 +0200

    feat(homepage): filter op

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,7 +1,12 @@
-export function FilterBar() {
+export function FilterBar(props) {
   return (
-    <>
-      <h2 className="uppercase">Filter Bar !</h2>  
-    </>
+    <div className="flex justify-center py-4">
+      <input 
+        type="text" 
+        placeholder="Filter Pokemon..." 
+        value={props.filter}
+        onChange={(e) => props.onFilterChange(e.target.value)}
+        className="border-2 border-lime-400 py-1 px-3 rounded-xl w-[95%] text:text-lime-700 placeholder:text-sm placeholder:text-lime-700 hover:bg-lime-50 transition-all transition-300" />
+    </div>
   )
 }
--- src/pages/PokedexHomePage.jsx
+++ src/pages/PokedexHomePage.jsx
@@ -5,6 +5,14 @@ import { fetchPokemonList } from "../utils/pokemon-utils";
 
 export function PokedexHomePage() {
   const [pokemonList, setPokemonList] = useState(null);
+  const [pokemonFilter, setPokemonFilter] = useState('');
+  const filteredPokemonList = pokemonFilter
+    ? pokemonList.filter(pokemon => 
+      pokemon.name.toLowerCase()
+      .includes(pokemonFilter.toLowerCase()) 
+      || pokemon.types.some(type =>
+        type.toLowerCase().includes(pokemonFilter.toLowerCase())))
+    : pokemonList;
 
   async function loadPokemonList() {
     const pokemonListData = await fetchPokemonList();
@@ -18,11 +26,12 @@ export function PokedexHomePage() {
   return (
     <div>
       <section className="">
-        <FilterBar/>
+        <FilterBar filter={pokemonFilter} onFilterChange={setPokemonFilter}/>
+        {filteredPokemonList?.length === 0 && (<p className="pl-10">Aucun résultat pour la recherche {pokemonFilter}</p>)}
       </section>
       <section>
         {pokemonList && (
-          <PokemonCardContainer pokemonList={pokemonList}/>
+          <PokemonCardContainer pokemonList={filteredPokemonList}/>
         )}
       </section>
     </div>
