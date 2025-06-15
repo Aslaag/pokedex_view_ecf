5264a086f7dc407dd0cbadea6a7ef63acbec03fd
Aslaag <lilice.flo@gmail.com>
2025-06-15 17:16:40 +0200

    feat(pokemonView): Stats witw percentages

--- src/components/StatBar.jsx
+++ src/components/StatBar.jsx
@@ -1,4 +1,16 @@
+import { MAX_STAT } from "../constants/stats-constants";
+
 export function StatBar(props) {
+  const statTraduction = {
+    "HP": "hp",
+    "Attack": "attack",
+    "Defense": "defense",
+    "Special attack": "specialAttack",
+    "Special defense": "specialDefense",
+    "Speed": "speed",
+  }
+  const fillBar = props.value * 100 / `${MAX_STAT[statTraduction[props.name]]}`;
+
   return (
     <div className="flex gap-1">
       <div className="flex gap-4 w-full">
@@ -6,7 +18,9 @@ export function StatBar(props) {
         <div className="w-2/3 flex items-center gap-1">
           <p>{props.value}</p>
           <span className="relative w-52 h-3 border-1 bg-gray-200 rounded-2xl">
-            <span className="absolute w-1/2 h-2.5 bg-blue-500 rounded-l-2xl rounded-r-lg"/>
+            <span 
+              className="absolute h-2.5 bg-blue-500 rounded-l-2xl rounded-r-lg"
+              style={{ width: `${fillBar}%` }}/>
           </span>
         </div>
       </div>
--- src/constants/stats-constants.jsx
+++ src/constants/stats-constants.jsx
@@ -1,8 +1,8 @@
 export const MAX_STAT = {
+hp: 105,
 attack: 130,
 defense: 180,
 specialAttack: 135,
 specialDefense: 120,
 speed: 120,
-hp: 105,
 };
