ea3881f9f64315c397235e157dd1abd491197591
Aslaag <lilice.flo@gmail.com>
2025-06-15 14:01:49 +0200

    feat(header): navigate to homePage logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router";
+import { ROUTES } from "../routes/Routes";
 import { Logo } from "./Logo";
 
 export function Header() {
+  let navigate = useNavigate()
    
   return (
-    <div className="bg-lime-800/60 p-2">
+    <div 
+      onClick={() => navigate(ROUTES.HOME)} 
+      className="bg-lime-800/60 p-2 hover:cursor-pointer">
       <Logo/>
     </div>
   )
